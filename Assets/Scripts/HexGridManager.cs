9841f9a63acacad874314fcf9ceabec7cd60e210
Joseph Merdrignac <joseph.merdrignac@gmail.com>
2021-09-03 18:35:49 +0200

    PopulateCellsFromScene + other changes

--- Assets/Scripts/HexGridManager.cs
+++ Assets/Scripts/HexGridManager.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+
 using UnityEngine;
 #if UNITY_EDITOR
 using UnityEditor;
@@ -8,36 +10,46 @@ using UnityEditor;
 public class HexGridManager : MonoBehaviour
 {
 
-    public GameObject hexCellSource;
+    public HexCell hexCellSource;
     public int rowCount = 8;
     public int columnCount = 12;
-    private ArrayList cells = new ArrayList();
+    
+    private List<HexCell> cells = new List<HexCell>();
     private Vector3 LeftBottomPoint =  new Vector3(-18,0,-8);
     private Vector3 LeftBottomPoint2 =  new Vector3(-16.5f,0,-7);
 
+    private void CreateHexCell(Vector3 position) {
+        var cell = Instantiate(hexCellSource, position, Quaternion.identity);
+        // cell.gameObject.hideFlags = HideFlags.HideInHierarchy;
+        cells.Add(cell);
+    }
 
     public void Build() {
-        
+        Clear();
         if (hexCellSource == null) {
-            Debug.LogError("Cmon you forget to provide the hex cell source");
+            Debug.LogError("C'mon you forget to provide the hex cell source");
         } else {
             for (int j = 0; j <= rowCount; j++) {
                 for (int i = 0; i <= columnCount; i++) {
                     Vector3 position = LeftBottomPoint + new Vector3(3*i,0,2*j);
-                    this.cells.Add(Instantiate(hexCellSource, position, Quaternion.identity));
+                    CreateHexCell(position);
                     if( i < columnCount && j < rowCount) {
-                    Vector3 position2 = LeftBottomPoint2 + new Vector3(3*i,0,2*j);
-                    this.cells.Add(Instantiate(hexCellSource, position2, Quaternion.identity));
+                        Vector3 position2 = LeftBottomPoint2 + new Vector3(3*i,0,2*j);
+                        CreateHexCell(position2);
                     }
                 }
             }
         }
     }
 
+    public void PopulateCellsFromScene() {
+        cells = FindObjectsOfType<HexCell>().ToList();
+    }
+
     public void Clear() {
-        foreach (GameObject item in this.cells)
-        {
-            DestroyImmediate(item);
+        PopulateCellsFromScene();
+        foreach (var item in this.cells) {
+            DestroyImmediate(item.gameObject);
         }
         this.cells.Clear();
     }
